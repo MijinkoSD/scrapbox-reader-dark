797a7c2b11a08e792379c74d5c24197dfef61b89
MijinkoSD <79152329+MijinkoSD@users.noreply.github.com>
2022-08-07 21:13:53 +0900

    Add support for Font Awesome

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import '../assets/app.css'
+import '../assets/app.scss'
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       <meta name="robots" content="noindex, nofollow" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"></link>
     </Head>
     <Component {...pageProps} />
   </>
